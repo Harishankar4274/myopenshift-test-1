fdea19e888998d07b9db821f7a712d160408c1e3
Harishankar4274 <harishankardubey2911@gmail.com>
2022-11-25 22:18:53 +0530

    version 3

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 print("this is my first php project ...");
 print("this is my sec php project ...");
 print `cat /etc/os-release`;
-print `apt update ; apt install net-tools -y`;
+print `whoami`;
+print `yum update ; yum install net-tools -y`;
 print `ifconfig`;
 ?>
 
