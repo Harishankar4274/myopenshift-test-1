67e0e3c625fc4bae92b8eae93a137bcbd0e369f6
Harishankar4274 <harishankardubey2911@gmail.com>
2022-11-25 21:05:49 +0530

    install ifconfig

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <?php
 print("this is my first php project ...");
 print("this is my sec php project ...");
+print `cat /etc/os-release`;
+print `apt update ; apt install net-tools -y`;
 print `ifconfig`;
 ?>
 
