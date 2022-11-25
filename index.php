482fd7157f357c4b92ba70335a7bfdaba082d798
Harishankar4274 <harishankardubey2911@gmail.com>
2022-11-25 22:31:08 +0530

    updated

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 print("this is my first php project ...");
 print("this is my sec php project ...");
 print `cat /etc/os-release`;
-print `whoami`;
+print `echo "Now we will print the username"; whoami`;
 print `yum update ; yum install net-tools -y`;
 print `ifconfig`;
 ?>
